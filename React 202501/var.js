f47a182af48228bb8a52a7e4884d1be2329c1dd3
Shades-of-CADELA <akindejiadelakun@gmail.com>
2025-03-05 21:22:03 +0100

    Initial commit

---
+++ React 202501/var.js
@@ -0,0 +1,11 @@
+var name = 'Deji';
+let age = 40
+const state = 'Kaduna'
+
+console.log(name, age, state)
+
+function greet(name){
+    return `Hello ${name}!`;
+}
+
+console.log (greet('Catherine'))
