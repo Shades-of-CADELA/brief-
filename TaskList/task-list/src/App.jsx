b95b2dbd6745386f8ce39c44b150e9b421f2780c
Shades-of-CADELA <akindejiadelakun@gmail.com>
2025-03-17 12:15:30 +0100

    Update App.jsx

--- TaskList/task-list/src/App.jsx
+++ TaskList/task-list/src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>I love Catherine</h1>
+      <h1>I love God</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
